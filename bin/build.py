aaf0555c5f9293a141a6c30bf3c92370e13bf822
DKOLABS <dillon.obrien@dkolabs.com>
2024-06-09 14:23:55 -0400

    beautiful soup and more clean up

--- bin/build.py
+++ bin/build.py
@@ -1,40 +1,29 @@
+import jinja2
 import yaml
 import random
 from pathlib import Path
-from datetime import datetime
+from bs4 import BeautifulSoup as bs
 
 
-def load_yaml_files(directory):
-    cards = []
-    for item in directory.rglob("*"):
-        if item.is_file():
-            with open(item, "r") as file:
-                card = yaml.safe_load(file)
-                card["source"] = "/".join(item.parts[1:])
-                cards.append(card)
-    return cards
-
-
-def read_template_file(filename):
-    with open(filename, "r") as file:
-        return file.read()
-
-
+# Function to generate a random hex color
 def generate_random_color():
     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 
+# Function to calculate the luminance of a hex color
 def calculate_luminance(hex_color):
-    hex_color = hex_color.lstrip('#')
+    hex_color = hex_color.lstrip("#")
     r, g, b = int(hex_color[0:2], 16), int(hex_color[2:4], 16), int(hex_color[4:6], 16)
     return 0.2126 * r + 0.7152 * g + 0.0722 * b
 
 
+# Function to determine if a color is readable on a black background
 def is_readable_on_black(hex_color):
     luminance = calculate_luminance(hex_color)
     return luminance > 150  # Adjust this threshold as needed
 
 
+# Function to generate a random color that is readable on a black background
 def generate_readable_color():
     while True:
         color = generate_random_color()
@@ -42,68 +31,100 @@ def generate_readable_color():
             return color
 
 
-def generate_html(cards):
-    tags = {tag for card in cards for tag in card.get("tags", [])}
-    tag_colors = {tag: generate_readable_color() for tag in tags}
+# Function to load YAML files from a directory, extracting relevant data
+def load_yaml_files(yaml_folder):
+    cards = list()
+    tags = set()
+    card_names = list()
 
-    styles = read_template_file(Path("templates/style.css"))
-    header_template = read_template_file(Path("templates/header.html"))
-    footer_template = read_template_file(Path("templates/footer.html"))
-    card_template = read_template_file(Path("templates/card.html"))
-    scripts = read_template_file(Path("templates/scripts.js"))
+    for file in yaml_folder.rglob("*.yaml"):
+        with open(file, "r") as file:
+            card = yaml.safe_load(file)
+        card["source"] = file.name.replace("data\\", "")
+        card["search_rows"] = card["search"].count("\n") + 1
+        card["tags_string"] = " ".join(card["tags"])
+        cards.append(card)
+        tags.update(card["tags"])
+        card_names.append(card["name"])
 
-    css_styles = styles
-    for tag, color in tag_colors.items():
-        css_styles += f"""
-.card .tag.{tag} {{ background-color: {color}; color: #000000; }}
-        """
-
-    tags_js = f"const tags = {list(tags)};"
-
-    scripts = scripts.replace("const tags = [];", tags_js)
-
-    header = header_template.replace(
-        '<link rel="stylesheet" href="styles.css">', f"<style>{css_styles}</style>"
-    )
-    footer = footer_template.replace(
-        '<script src="scripts.js"></script>', f"<script>{scripts}</script>"
-    )
-
-    cards_html = ""
-    for card in cards:
-        tags = " ".join(card.get("tags", []))
-        tags_html = " ".join([f'<span class="tag {tag}">{tag}</span>' for tag in card.get("tags", [])])
-        search = card.get("search")
-        rows = search.count("\n") + 1  # Calculate the number of lines
-        card_html = card_template.format(
-            name=card.get("name"),
-            tags=tags,
-            tags_html=tags_html,
-            description=card.get("description"),
-            search=search,
-            rows=rows,
-            author=card.get("author"),
-            last_updated=card.get("last_updated"),
-            version=card.get("version"),
-            id=card.get("id"),
-            source=card.get("source")
-        )
-        cards_html += card_html
-
-    return header + cards_html + footer
+    return cards, tags, sorted(card_names)
 
 
-def main():
-    input_directory = Path("../data/")
-    # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    timestamp = "test"
-    outfile_str = Path(f"../build/index_{timestamp}.html")
-    cards = load_yaml_files(input_directory)
-    html_content = generate_html(cards)
-
-    with open(outfile_str, "w") as file:
-        file.write(html_content)
+# Function to render a Jinja2 template with the given context
+def render_template(template_path, context):
+    template_loader = jinja2.FileSystemLoader(searchpath=template_path.parent)
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template(template_path.name)
+    return template.render(context)
 
 
 if __name__ == "__main__":
-    main()
+
+    # Paths to YAML files and temporary folder
+    yaml_files = Path("./data/")
+    temp_folder = Path("./build/temp/")
+
+    # Paths to Jinja2 templates
+    header_template = Path("./bin/templates/header.html")
+    footer_template = Path("./bin/templates/footer.html")
+    card_template = Path("./bin/templates/card.html")
+
+    ##########################
+    ##### Generate Cards #####
+    ##########################
+
+    # Load cards, tags, and card names from YAML files
+    cards, tags, card_names = load_yaml_files(yaml_files)
+
+    # Generate a readable color for each tag
+    tag_colors = {tag: generate_readable_color() for tag in tags}
+
+    # Generate HTML for each card
+    cards_html = str()
+    for card_name in card_names:
+        card_data = [d for d in cards if d.get("name") == card_name][0]
+        cards_html += render_template(card_template, card_data)
+
+    ###########################
+    ##### Generate Header #####
+    ###########################
+
+    # Load CSS styles from file
+    with open("./bin/templates/style.css", "r") as file:
+        css_styles = file.read()
+
+    # Add tag styles to CSS
+    for tag, color in tag_colors.items():
+        css_styles += (
+            f".card .tag.{tag} {{ background-color: {color}; color: #000000; }}\n"
+        )
+
+    css_styles = {"style": css_styles}
+    header_html = render_template(header_template, css_styles)
+
+    ###########################
+    ##### Generate Footer #####
+    ###########################
+
+    # Load JavaScript from file
+    with open("./bin/templates/scripts.js", "r") as file:
+        scripts = file.read()
+
+    # Add tags to JavaScript
+    tags_js = f"const tags = {sorted(tags)};"
+    scripts = scripts.replace("const tags = [];", tags_js)
+
+    scripts = {"scripts": scripts}
+    footer_html = render_template(footer_template, scripts)
+
+    #######################
+    ##### Create File #####
+    #######################
+
+    # Combine header, cards, and footer into a single HTML file
+    html = header_html + cards_html + footer_html
+    # with open(Path("./build/out.html"), "w") as out_file:
+    #     out_file.write(html)
+
+    with open(Path("./build/out_bs.html"), "w") as out_file:
+        out_file.write(bs(html, "html.parser").prettify())
